c2c5f4b5b47caa7494fee5ebeebc0c5ac4c9fcb6
Duong Nguyen <andy@vmi1727826.contaboserver.net>
2024-03-16 23:21:25 +0100

    Fix webhosting and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.10.9-slim
 
 ARG UNAME=andy
-ARG UID=1003
-ARG GID=1004
+ARG UID=1000
+ARG GID=1000
 RUN groupadd -g $GID -o $UNAME
 RUN useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME
 
@@ -18,14 +18,14 @@ RUN pip install -r requirements.txt
 RUN python -m playwright install
 RUN python -m playwright install-deps
 
+
+RUN playwright install
+
 RUN mkdir /app
 ADD . /app
 WORKDIR /app
-RUN chmod 755 /app
-RUN chown andy:andy /app
 #RUN rm config.toml && cp config_default.toml config.toml
 # tricks for pytube : https://github.com/elebumm/RedditVideoMakerBot/issues/142 
 # (NOTE : This is no longer useful since pytube was removed from the dependencies)
 # RUN sed -i 's/re.compile(r"^\\w+\\W")/re.compile(r"^\\$*\\w+\\W")/' /usr/local/lib/python3.8/dist-packages/pytube/cipher.py
-USER $UNAME
 CMD ["python3", "main.py"]
--- webhost.py
+++ webhost.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask, render_template, request, send_file
+import subprocess
+import threading
+import time
+import toml
+
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    root_dir = os.getcwd()  # Current working directory
-    return render_template('index.html', root=root_dir)
 
 @app.route('/browse')
 def browse():
@@ -26,5 +26,63 @@ def download_file():
     filepath = request.args.get('filepath', '')
     return send_file(filepath, as_attachment=True)
 
+def process_docker():
+    # Trigger docker process
+    command = "docker run --rm -v /home/andy/repo/reddit-video:/app reddit-video:1.3"
+    subprocess.run(command, shell=True)
+
+    # Wait for docker process to complete
+    time.sleep(1200)  # Assuming 20 minutes processing time
+
+    # Check if results file is created
+    if os.path.exists("results/processed_file.txt"):
+        return "Processing succeeded!"
+    else:
+        return "Processing failed!"
+
+
+def update_config(subreddit, post_id):
+    config_path = 'config.toml'
+    if not os.path.exists(config_path):
+        return "Config file not found."
+
+    with open(config_path, 'r') as f:
+        config = toml.load(f)
+
+    reddit_config = config.get('reddit')
+    if not reddit_config:
+        return "reddit configuration not found in the config file."
+
+    thread_config = reddit_config.get('thread')
+    if not thread_config:
+        reddit_config['thread'] = {}
+
+    reddit_config['thread']['subreddit'] = subreddit
+    reddit_config['thread']['post_id'] = post_id
+
+    with open(config_path, 'w') as f:
+        toml.dump(config, f)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        subreddit = request.form['subreddit']
+        post_id = request.form['post_id']
+
+        # Update config.toml
+        update_config(subreddit, post_id)
+
+        # Start docker process in a separate thread
+        docker_thread = threading.Thread(target=process_docker)
+        docker_thread.start()
+
+        return render_template('index.html', message="Processing started! Please wait.")
+
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port=8080, debug=False)
+
+
+
